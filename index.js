729dcc41a37ba52268d3f994c62c13a4bf77a711
Vitor Monteiro <bitoiu@github.com>
2020-02-25 15:06:45 +0100

    basic test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 try {
   const orgName = core.getInput('org');
   console.log(`Org input: ${orgName}!`);  
+  core.setOutput("org", $orgName);
 } catch (error) {
   core.setFailed(error.message);
 }
