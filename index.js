ff41462a3792e29f1644e0e0be4853520ebac89c
Sebass van Boxel <svboxel@gmail.com>
2020-02-25 16:34:38 +0100

    Graphql pagination

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { graphql } = require("@octokit/graphql");
 
-const main = async () => {
-  const output = [];
+const main = async (data = null, collaboratorsCursor = null, repositoriesCursor = null) => {
   const graphqlWithAuth = graphql.defaults({
     headers: {
       authorization: `token ${process.env.TOKEN}`
     }
   });
+
   const { organization } = await graphqlWithAuth(`
-    query ($organization: String!) {
+    query ($organization: String!, $collaboratorsCursor: String, $repositoriesCursor: String) {
       organization(login: $organization) {
-        repositories (first: 10) {
+        repositories (first: 1, after: $repositoriesCursor) {
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
           nodes {
             name
-            collaborators(first: 10, affiliation: ALL) {
+            collaborators(first: 100, after: $collaboratorsCursor, affiliation: ALL) {
+              pageInfo {
+                endCursor
+                hasNextPage
+              }
               edges {
                 cursor
                 node {
@@ -29,13 +37,34 @@ const main = async () => {
         }
       }
     }
-  `, {
+  `, 
+  {
     organization: 'octodemo',
+    collaboratorsCursor,
+    repositoriesCursor
   });
-  console.log(organization)
+
+  collaboratorsPage = organization.repositories.nodes[0].collaborators;
+
+  while(collaboratorsPage.pageInfo.hasNextPage === true) {
+    await main(
+      organization, 
+      collaboratorsPage.pageInfo.endCursor
+    )
+  }
+
+  repositoriesPage = organization.repositories;
+  while(repositoriesPage.pageInfo.hasNextPage === true) {
+    await main(
+      organization,
+      null,
+      repositoriesPage.pageInfo.endCursor
+    )
+  }
 }
 try {
   main();
 } catch (error) {
   core.setFailed(error.message);
 }
+
