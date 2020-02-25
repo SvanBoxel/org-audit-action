15ea4a6cafbadb6c83b9dce5c99741be136053de
Sebass van Boxel <svboxel@gmail.com>
2020-02-25 15:48:27 +0100

    initial graphql query

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { graphql } = require("@octokit/graphql");
 
+const main = async () => {
+  const output = [];
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${process.env.TOKEN}`
+    }
+  });
+  const { organization } = await graphqlWithAuth(`
+    query ($organization: String!) {
+      organization(login: $organization) {
+        repositories (first: 10) {
+          nodes {
+            name
+            collaborators(first: 10, affiliation: ALL) {
+              edges {
+                cursor
+                node {
+                  name
+                  email
+                }
+                permission
+              }
+            }
+          }
+        }
+      }
+    }
+  `, {
+    organization: 'octodemo',
+  });
+  console.log(organization)
+}
 try {
-  const orgName = core.getInput('org');
-  console.log(`Org input: ${orgName}!`);  
-  core.setOutput("org", orgName);
+  main();
 } catch (error) {
   core.setFailed(error.message);
 }
